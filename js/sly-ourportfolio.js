046d616efb78a079a4a9920cfeaa73624b5aff53
MUHSIN BIN MUHAMMED MUNEER <49632623+MuhsinMuneer@users.noreply.github.com>
2021-05-06 14:24:54 +0530

    js

---
+++ js/sly-ourportfolio.js
@@ -0,0 +1,1 @@
+(function(){'use strict';var $frame=$('#forcecentered');var $wrap=$frame.parent();$frame.sly({horizontal:1,itemNav:'basic',smart:1,activateMiddle:1,activateOn:'click',mouseDragging:1,touchDragging:1,releaseSwing:1,startAt:0,scrollBar:$wrap.find('.scrollbar'),scrollBy:0,speed:300,elasticBounds:1,easing:'easeOutExpo',dragHandle:1,dynamicHandle:1,clickBar:1});}());
